4d76cc26ae7b32b92a5d53ad0fc4722a03b0acbe
root <root@localhost.localdomain>
2018-02-27 09:50:42 +0100

    copying all onet

---
+++ onet/app/server_test.go
@@ -0,0 +1,21 @@
+package app
+
+import (
+	"testing"
+
+	"io/ioutil"
+	"os"
+
+	"github.com/dedis/kyber/suites"
+	"github.com/dedis/student_18_dgcosi/onet/log"
+)
+
+func TestInteractiveConfig(t *testing.T) {
+	tmp, err := ioutil.TempDir("", "conode")
+	log.ErrFatal(err)
+	log.OutputToBuf()
+	setInput("127.0.0.1:2000\nConode1\n" + tmp)
+	InteractiveConfig(suites.MustFind("Ed25519"), tmp+"/config.bin")
+	log.ErrFatal(os.RemoveAll(tmp))
+	log.OutputToOs()
+}
